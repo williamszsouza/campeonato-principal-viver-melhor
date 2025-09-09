8f8a126bbbfaa6d5225175d04a5838b1d876d3d4
williamszsouza <biologobiologo1@gmail.com>
2025-09-09 20:17:23 -0300

    adicionando carousel de patrocinadores e suas imagens

--- src/views/header.vue
+++ src/views/header.vue
@@ -29,41 +29,59 @@
 </template>
 
 <script>
-import pat1 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 1.png';
-import pat2 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 2.png';
-import pat3 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 3.png';
-import pat4 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 4.png';
-import pat5 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 5.png';
-import pat6 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 6.png';
-import pat7 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 7.png';
-import pat8 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 8.png';
-import pat9 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 9.png';
-import pat10 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 10.png';
-import pat11 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 11.png';
-import pat12 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 12.png';
-import pat13 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 13.png';
-import pat14 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 14.png';
-import pat15 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 15.png';
-import pat16 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 16.png';
-import pat17 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 17.png';
-import pat18 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 18.png';
-import pat19 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 19.png';
-import pat20 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 20.png';
-import pat21 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 21.png';
-import pat22 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 22.png';
-import pat23 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 23.png';
-import pat24 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 24.png';
-import pat25 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 25.png';
-import pat26 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 26.png';
-import pat27 from '../assets/patrocinios/Imagem do WhatsApp de 2025-09-08 à(s) 14.45.17_aa142602 27.png';
+import pat100 from '../assets/patrocinios/pat100.png';
+import pat101 from '../assets/patrocinios/pat101.png';
+import pat102 from '../assets/patrocinios/pat102.png';
+import pat103 from '../assets/patrocinios/pat103.png';
+import pat104 from '../assets/patrocinios/pat104.png';
+import pat105 from '../assets/patrocinios/pat105.png';
+import pat106 from '../assets/patrocinios/pat106.png';
+import pat107 from '../assets/patrocinios/pat107.png';
+import pat108 from '../assets/patrocinios/pat108.png';
+import pat109 from '../assets/patrocinios/pat109.png';
+import pat110 from '../assets/patrocinios/pat110.png';
+import pat111 from '../assets/patrocinios/pat111.png';
+import pat112 from '../assets/patrocinios/pat112.png';
+import pat113 from '../assets/patrocinios/pat113.png';
+import pat114 from '../assets/patrocinios/pat114.png';
+import pat115 from '../assets/patrocinios/pat115.png';
+import pat116 from '../assets/patrocinios/pat116.png';
+import pat117 from '../assets/patrocinios/pat117.png';
+import pat118 from '../assets/patrocinios/pat118.png';
+import pat119 from '../assets/patrocinios/pat119.png';
+import pat120 from '../assets/patrocinios/pat120.png';
+import pat121 from '../assets/patrocinios/pat121.png';
+import pat122 from '../assets/patrocinios/pat122.png';
+import pat123 from '../assets/patrocinios/pat123.png';
+import pat124 from '../assets/patrocinios/pat124.png';
+import pat125 from '../assets/patrocinios/pat125.png';
+import pat126 from '../assets/patrocinios/pat126.png';
+import pat127 from '../assets/patrocinios/pat127.png';
+import pat128 from '../assets/patrocinios/pat128.png';
+import pat129 from '../assets/patrocinios/pat129.png';
+import pat130 from '../assets/patrocinios/pat130.png';
+import pat131 from '../assets/patrocinios/pat131.png';
+import pat132 from '../assets/patrocinios/pat132.png';
+import pat133 from '../assets/patrocinios/pat133.png';
+import pat134 from '../assets/patrocinios/pat134.png';
+import pat135 from '../assets/patrocinios/pat135.png';
+import pat136 from '../assets/patrocinios/pat136.png';
+import pat137 from '../assets/patrocinios/pat137.png';
+import pat138 from '../assets/patrocinios/pat138.png';
+import pat139 from '../assets/patrocinios/pat139.png';
+import pat140 from '../assets/patrocinios/pat140.png';
+import pat141 from '../assets/patrocinios/pat141.png';
+import pat142 from '../assets/patrocinios/pat142.png';
 
 export default {
   data() {
     return {
       patrocinadores: [
-        pat1, pat2, pat3, pat4, pat5, pat6, pat7, pat8, pat9, pat10,
-        pat11, pat12, pat13, pat14, pat15, pat16, pat17, pat18, pat19, pat20,
-        pat21, pat22, pat23, pat24, pat25, pat26, pat27
+        pat100, pat101, pat102, pat103, pat104, pat105, pat106, pat107, pat108, pat109,
+        pat110, pat111, pat112, pat113, pat114, pat115, pat116, pat117, pat118, pat119,
+        pat120, pat121, pat122, pat123, pat124, pat125, pat126, pat127, pat128, pat129,
+        pat130, pat131, pat132, pat133, pat134, pat135, pat136, pat137, pat138, pat139,
+        pat140, pat141, pat142
       ],
       offset: 0,
       speed: 1
@@ -90,6 +108,7 @@ export default {
   }
 };
 </script>
+
 
 <style scoped>
 * {
@@ -169,7 +188,8 @@ export default {
 
 .patro-card img {
   max-width: 100%;
-  max-height: 80px;
+  max-height: 150px;
+  image-rendering: optimizeQuality;
   object-fit: cover;
 }
 
